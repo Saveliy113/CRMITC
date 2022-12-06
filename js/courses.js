20345f2a2d4033b273d369e029e0c12b3abaff74
rustamsr05 <rustamsr05@gmail.com>
2022-12-06 11:57:14 +0600

    added course add window

--- js/courses.js
+++ js/courses.js
@@ -1,18 +1,59 @@
-fetch('http://165.22.49.123:5000/api/v1/mainapp/course/')
+const table = document.querySelector('.table__body');
+// const addButton = document.querySelector('.add-button');
+const addCourseForm = document.querySelector('.form-container')
+
+const token = '27c71e5ad1261aa245f3481d9e57f50dc99e13a4';
+
+const headers = {
+  "Content-type": "application/json",
+  "Authorization": `Token ${token}`
+}
+
+fetch('http://165.22.49.123:5000/api/v1/mainapp/course/', {
+  headers: headers
+})
   .then(response => response.json())
-  .then(result => console.log(result));
+  .then(data => {
+    console.log(data)
+    data.map(item => {
+      table.innerHTML += 
+      `
+      <tr>
+        <td>${item.id}</td>
+        <td>${item.title}</td>
+        <td>${item.date_start}</td>
+        <td>${item.finish_date}</td>
+        <td>${item.price}</td>
+      </tr>
+      `
+   });
+  });
+
+// fetch('/login_ajax.php', {
+//   method: 'POST',
+//   body: params
+// }).then(
+//   response => {
+//      return response.text();
+//   }
+// ).then(
+//   text => {
+//      document.getElementById('result').innerHTML = text;
+//   }
+// );
 
 
-  // function getModal(id) {
-  //   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     modal.style.display = 'flex';
-  //     modal.innerHTML = `
-  //       <h2 class="recipe-modal__title">${data.meals[0].strMeal}</h2>
-  //       <p class="recipe-modal__text">${data.meals[0].strInstructions}</p>
-  //       <img src="${data.meals[0].strMealThumb}" alt="meal image" class="recipe-modal__img">
-  //       <button class="recipe-modal__close-button" onclick="modalClose()">CLOSE</button>
-  //     `;
-  // })
-  // }
+  function addCourseOpen() {
+    addCourseForm.classList.remove('visually-hidden');
+  }
+
+  function addCourseClose() {
+    addCourseForm.classList.add('visually-hidden');
+  }
+
+  function getValue() {
+    console.log(enent.target.value);
+  }
+
+
+  
